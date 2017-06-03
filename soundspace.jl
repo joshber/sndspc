fdab405a96997651a93fb5fb0e311c4cfee943f5
joshber <joshwa.berson@gmail.com>
2017-06-03 13:29:29 +0200

    ...

--- soundspace.jl
+++ soundspace.jl
@@ -1,4 +1,9 @@
 #=
+FIXME--
+- Fewer SF features -- ≤12KHz, ≥200?, more fband resolution, use spectrogram defaults
+- Cut feature vector to suit fs
+=#
+#=
 soundpattern.jl: Pattern Spaces for Sound
 
 Josh Berson, josh@joshberson.net
@@ -13,13 +18,14 @@ Why:
   features of sound and observing how they change over time
 
 Inspirations:
+- Nadav Hochman's work on style spaces for image social media
 - Dupont and Ravet, Improved audio classification using a novel non-linear
   dimensionality reduction ensemble approach (2013)
 =#
 
 using StatsBase, MultivariateStats
 using WAV, DSP, MFCC
-#using GR # ObjectiveFunction needs v0.6
+#using GR, GLVisualize # ObjectiveFunction needs v0.6
 using Plots
 
 
@@ -36,6 +42,7 @@ function extractSamples( path, len = 3, step = len/2 )
   step = Int64(floor(step * fs))
 
   # Extract samples of length len at intervals step
+  # Array{Float64, 2}: stereo sampling
   p = 1
   n = Int64(floor((size(source, 1) - (len - step)) / step))
   samples = Array{Array{Float64, 2}}(n)
@@ -47,7 +54,7 @@ function extractSamples( path, len = 3, step = len/2 )
   samples, fs
 end
 
-function extractFeatures( sample, fs )
+function extractFeatures( sample, fs, sfmin, sfmax )
   #=
   Generates a feature vector for the spectral envelope of an audio signal
   Features:
@@ -62,7 +69,8 @@ function extractFeatures( sample, fs )
   =#
 
   # See https://github.com/davidavdav/MFCC.jl
-  # Default is 63 filterbanks -- adjust with kwarg nbands
+  # Default is 63 filterbanks — adjust with kwarg nbands
+  # augtype=::delta means get first and second derivatives too
   mfc = feacalc(sample; sr=fs, augtype=:ddelta, sadtype=:none, normtype=:none, defaults=:wbspeaker, numcep=13)
   mfc = mfc[1] # Discard metadata
 
@@ -70,7 +78,7 @@ function extractFeatures( sample, fs )
   # Now get MFCC mean, variance, skewness, and kurtosis
 
   # Extract columns. Each column = framewise values for one coefficient
-  # size(cols) = 39
+  # size(cols) == 39
   cols = [ view(mfc,:,c) for c in 1:size(mfc,2) ]
 
   mfµ = mean.(cols)
@@ -85,13 +93,14 @@ function extractFeatures( sample, fs )
   # Spectral flatness
 
   mono = (sample[:,1] .+ sample[:,2]) ./ 2 # Average channels to get mono signal
-  spec = spectrogram(mono, 4096; fs=fs, window=hanning)
-    # 4096 frames per STFT (default is length(signal)/8); default overlap is .5
+  spec = spectrogram(mono; fs=fs, window=hanning)
+    # Default STFT frame span is length(signal)/8). Default overlap is .5
   pow = power(spec)
 
-  # Log-bin the power spectrum down to 20Hz
+  # Log-bin the power spectrum down to sfmin Hz
   fbands = size(pow,1)
-  binshifts = [ i for i in 1:12 if Int64(fs) >> i ≥ 20 && fbands >> i ≥ 1 ]
+  binshifts = logbins(fs, sfmin, sfmax)
+    # N.b., assert(fbands >> binshifts[end] ≥ 2) — no problem for reasonable sfmax
   nbins = length(binshifts)
   bins = Array{Array{Float64, 2}}(nbins)
   for i in 1:nbins
@@ -104,7 +113,7 @@ function extractFeatures( sample, fs )
   sfσ2 = Array{Float64}(nbins)
   sfγ = Array{Float64}(nbins)
   sfkur = Array{Float64}(nbins)
-  # For subarray b, rows = powers at different frequencies (F domain),
+  # For subarray bins[i], rows = powers at different frequencies (F domain),
   # cols = powers at a frequency at different points in time (T domain)
   for i in 1:nbins
     cols = [ view(bins[i],:,c) for c in 1:size(bins[i],2) ]
@@ -118,34 +127,42 @@ function extractFeatures( sample, fs )
   end
   # length(sfmean) == nbins, same for higher moments
 
-  # Return a feature vector
-  # With 13 MFCCs and 11 log bins, that's 200 features
   [ mfµ; mfσ2; mfγ; mfkur; sfµ; sfσ2; sfγ; sfkur ]
+end
+
+function logbins( fs, fmin, fmax )
+  b = map( i -> fmin ≤ Int64(fs / 2) >> i ≤ fmax, 1:11 )
+  [ i for i in 1:11 if b[i] ]
 end
 
 function constructFeatureSpace( samples, fs )
   #=
-  Dot notation would yield a vector of vectors
+  Dot vectorization would yield a vector of vectors
   What we need is a two-dimensional array
   =#
-
-  # TODO: Adjust # features according to fs
-  # 144 MFCC features + 4n where n = 11 for fs of 44.1KHz or higher
-  # but less for fs of 16 or 8KHz
-
-  X = Array{Float64, 2}(length(samples), 200)
+  sfmin, sfmax = 200, 12000
+  # 144 MFCC features + 4n for spectral flatness
+  # where n is # log bins with lower bound in [sfmin,sfmax]Hz
+  nfeatures = 144 + 4 * length(logbins(fs, sfmin, sfmax))
+  X = Array{Float64, 2}(length(samples), nfeatures)
   for i in 1:length(samples)
-    X[i, 1:end] = extractFeatures(samples[i], fs)[1:end]
+    X[i, 1:end] = extractFeatures(samples[i], fs, sfmin, sfmax)[1:end]
   end
   X
 end
 
 function main()
   sourcePath = "/Users/josh/Sync/Recordings/LaosFebMar2016/lp.wav"
-  
-  X = constructFeatureSpace(extractSamples(sourcePath)...)
-  ica = fit(ICA, X, 10)
-  # now plot ...
+
+  # TODO: Need higher resolution in the time domain, say step=.1s
+  # But that froze my MBA
+  X = constructFeatureSpace(extractSamples(sourcePath, 3, 1)...)
+  pca = fit(PCA, X, 10)
+  @show principalratio(pca)
+    # TODO: Select PCA dim to capture 95 percent of covariance
+
+  # TODO: KPCA with Gaussian kernel to capture nonlinearities in the feature space
+  # TODO: Plot — 3D animation of PCs against time, with PC value on vertical
 end
 
 main()
